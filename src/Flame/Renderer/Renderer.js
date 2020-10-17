822c63a4f0106b5c9eccd824fb135dc80f8560ec
Eduardo Asafe <e@asafe.dev>
2020-10-17 12:06:12 +0800

    Dont compare hook callback

--- src/Flame/Renderer/Renderer.js
+++ src/Flame/Renderer/Renderer.js
@@ -6,7 +6,6 @@ var patch = require('snabbdom').init([
 	require('snabbdom/modules/eventlisteners').default,
 ]),
 	h = require('snabbdom/h').default,
-	thunk = require('snabbdom/thunk').default,
 	toVNode = require('snabbdom/tovnode').default;
 
 exports.emptyVNode = [];
@@ -41,7 +40,51 @@ function runEvent(handler) {
 
 exports.h_ = h;
 
-exports.thunk_ = thunk;
+function copyToThunk(vnode, thunk) {
+    thunk.elm = vnode.elm;
+    vnode.data.fn = thunk.data.fn;
+    vnode.data.args = thunk.data.args;
+    thunk.data = vnode.data;
+    thunk.children = vnode.children;
+    thunk.text = vnode.text;
+    thunk.elm = vnode.elm;
+}
+
+function initThunkHook(thunk) {
+    var cur = thunk.data;
+    var vnode = cur.fn.apply(undefined, cur.args);
+    copyToThunk(vnode, thunk);
+}
+
+function prepatchThunkHook(oldVnode, thunk) {
+    var i, old = oldVnode.data, cur = thunk.data;
+    var oldArgs = old.args, args = cur.args;
+    if (oldArgs.length !== args.length) {
+        copyToThunk(cur.fn.apply(undefined, args), thunk);
+        return;
+    }
+    for (i = 0; i < args.length; ++i) {
+        if (oldArgs[i] !== args[i]) {
+            copyToThunk(cur.fn.apply(undefined, args), thunk);
+            return;
+        }
+    }
+    copyToThunk(oldVnode, thunk);
+}
+
+exports.thunk_ = function thunk(sel, key, fn, args) {
+    if (args === undefined) {
+        args = fn;
+        fn = key;
+        key = undefined;
+    }
+    return h(sel, {
+        key: key,
+        hook: { init: initThunkHook, prepatch: prepatchThunkHook },
+        fn: fn,
+        args: args
+    });
+};
 
 exports.patch_ = patch;
 
